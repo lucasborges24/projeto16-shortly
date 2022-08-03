f42bce4cc66ef1a4fb590f5c9efe5a80fc980810
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-03 11:49:19 -0300

    feat: add signupSchema & create database functions
    SignupSchema to data payload sent. A function to get emails using email as parameter. Finally, a function to insert in database a new user

---
+++ src/models/authModel.js
@@ -0,0 +1,29 @@
+import joi from "joi";
+import { connection } from "./index.js";
+import { queries } from "./index.js";
+
+// DATABASE
+
+export const emailAlreadyExists = async (email) => {
+  const { rows: user } = await connection.query(queries.getEmailByEmail(), [
+    email,
+  ]);
+  if (user && user.length !== 0) {
+    return true;
+  }
+  return false;
+};
+
+export const insertNewUser = async (user) => {
+  const { name, email, password } = user;
+  await connection.query(queries.insertInUsers(), [name, email, password]);
+};
+
+// SCHEMA
+
+export const signupSchema = joi.object({
+  name: joi.string().required().trim(),
+  email: joi.string().email().required().trim(),
+  password: joi.string().required(),
+  confirmPassword: joi.string().equal(joi.ref("password")).required(),
+});
