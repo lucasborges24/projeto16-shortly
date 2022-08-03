58d72068945f5b13e69a017b90e75f47c91450ef
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-03 11:52:36 -0300

    feat: create two queries
    The first one select an existing email. The second insert a new user in database

---
+++ src/models/queries.js
@@ -0,0 +1,22 @@
+export const getEmailByEmail = () => {
+  const query = `--sql
+    SELECT
+        email
+    FROM
+        users
+    WHERE
+        email = $1;             
+`;
+
+  return query;
+};
+
+export const insertInUsers = () => {
+  const query = `--sql
+        INSERT INTO users
+            ("name", "email", "password")
+        VALUES
+            ($1, $2, $3);        
+    `;
+  return query;
+};
