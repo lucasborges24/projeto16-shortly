e39d54128980d4daf7be2742d29a1863be3d16dd
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-03 12:52:19 -0300

    feat: add signin middlewares functions
    Validate body and check password/email sent in payload syncronized between them

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,8 +1,19 @@
 import { stripHtml } from "string-strip-html";
+import bcrypt from "bcrypt";
 
 import { authModel } from "../models/index.js";
 
-export const validateBody = (req, res, next) => {
+export const checkEmailAlreadyExist = async (req, res, next) => {
+  const { email } = res.locals.newUser;
+  const emailExists = await authModel.emailAlreadyExists(email);
+  if (emailExists) {
+    return res.sendStatus(409);
+  }
+  next();
+  return true;
+};
+
+export const validateSignUpBody = (req, res, next) => {
   const validateBody = authModel.signupSchema.validate(req.body);
   if (validateBody.error) {
     return res.status(422).send("Some error with JSON body");
@@ -27,11 +38,31 @@ export const validateBody = (req, res, next) => {
   return true;
 };
 
-export const checkEmailAlreadyExist = async (req, res, next) => {
-  const { email } = res.locals.newUser;
-  const emailExists = await authModel.emailAlreadyExists(email);
-  if (emailExists) {
-    return res.sendStatus(409)
+export const validateSignInBody = (req, res, next) => {
+  const validateBody = authModel.signinSchema.validate(req.body);
+  if (validateBody.error) {
+    return res.status(422).send("Some error with JSON body");
+  }
+  const user = {
+    email: validateBody.value.email,
+    password: validateBody.value.password,
+  };
+
+  res.locals.user = user;
+  next();
+  return true;
+};
+
+export const checkPasswordByEmail = async (req, res, next) => {
+  const { email, password } = res.locals.user;
+  const passwordCrypt = await authModel.getPasswordByEmail(email)
+  if (!passwordCrypt) {
+    return res.status(401).send("Email or password wrong!")
+  }
+  
+  const passwordIsValid = bcrypt.compareSync(password, passwordCrypt)
+  if (!passwordIsValid) {
+    return res.status(401).send("Email or password wrong!")
   }
   next();
   return true;
