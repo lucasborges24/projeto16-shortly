31b3294f0dfb4f41f764f4fda915de90d290d904
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-03 10:15:58 -0300

    chore: create auth architeture files

---
+++ src/routes/authRoute.js
@@ -0,0 +1,12 @@
+import {Router} from "express";
+
+// import { authMiddleware } from "../middlewares/index.js";
+// import { authController } from "../controllers/index.js";
+
+const authRoutes = Router();
+
+// authRoutes.get('/login', 'somefunction')
+
+export {
+    authRoutes
+}
