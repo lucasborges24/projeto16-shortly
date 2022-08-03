7542a6f977271720f14272a016ad1bfb017f4275
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-03 10:13:28 -0300

    fix: uncomment routes use
    They were comment because I hadn't created the routes yet

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-// import router from "./routes/index.js"
+import router from "./routes/index.js"
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-//app.use(router)
+app.use(router)
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
