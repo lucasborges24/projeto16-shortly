ba71b6b5a77e3fbda320396ff4c9a350855c5a2a
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-04 16:41:52 -0300

    feat: validate token comming from header
    This is done with middleware validateHeader function

--- src/middlewares/urlMiddleware.js
+++ src/middlewares/urlMiddleware.js
@@ -3,14 +3,29 @@ import { urlModel } from "../models/index.js";
 export const validateUrlBody = (req, res, next) => {
   const validate = urlModel.urlSchema.validate(req.body);
   if (validate.error) {
-    return res.status(422).send("Some error with JSON body");
+    return res.status(422).send(`Some error with JSON body: ${validate.error.message}`);
   }
 
   const url = {
     url: validate.value.url,
   };
-  
+
   res.locals.url = url;
   next();
   return true;
 };
+
+export const validateHeader = (req, res, next) => {
+    const headerValidation = urlModel.tokenSchema.validate(req.headers)
+    if (headerValidation.error) {
+        return res.status(401).send(`Invalid token: ${headerValidation.error.message}`)
+    }
+
+    const token = {
+        token: headerValidation.value.authorization.split(" ")[1]
+    }
+
+    res.locals.token = token
+    next();
+    return true;
+}
