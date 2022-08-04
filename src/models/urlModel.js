42926d98fe09ad51fe5b8199752dafd09868dc43
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-04 13:12:06 -0300

    feat: create schema to validate an url

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -5,3 +5,12 @@ import { queries } from "./index.js";
 // DATABASE
 
 // SCHEMA
+
+export const urlSchema = joi.object({
+  url: joi
+    .string()
+    .pattern(
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
+    )
+    .required(),
+});
