dd8b1e0413ce124b636537555d82108c3d5cf1e6
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-05 10:43:51 -0300

    feat: create models functions
    Insert a document in urls and urlsUsers tables. Get info from urls by shortUrl and from urlsUsers by url and userId

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -4,6 +4,39 @@ import { queries } from "./index.js";
 
 // DATABASE
 
+export const insertInUrls = async (urlObject) => {
+  const { shortUrl, url } = urlObject;
+  const inserted = await connection.query(queries.insertUrl(), [url, shortUrl]);
+  if (inserted) return true;
+  return false;
+};
+
+export const getUrlIdByShortUrl = async (shortUrl) => {
+  const { rows: id } = await connection.query(queries.getUrlIdbyshortUrl(), [
+    shortUrl,
+  ]);
+  if (id && id.length !== 0) {
+    return id[0].id;
+  }
+  return false;
+};
+
+export const getUrlsUsersIdByUrlAndUserId = async (userId, url) => {
+  const { rows: id } = await connection.query(queries.getUrlsUsersIdByUserIdAndUrl(), [url, userId]);
+  if (id && id.length !== 0) {
+    return id[0].id;
+  }
+  return false;
+};
+
+export const insertInUrlsUsers = async (urlId, userId) => {
+  const inserted = await connection.query(queries.insertUrlsUsers(), [
+    urlId,
+    userId,
+  ]);
+  if (inserted) return true;
+  return false;
+};
 
 // SCHEMA
 
