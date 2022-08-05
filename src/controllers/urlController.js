661bd94d448356b4f90d6fdb47bd102a7a80adc8
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-05 10:35:45 -0300

    feat: create function to POST urls
    The function receive an url and response a shortUrl, in addiction to send them to database

---
+++ src/controllers/urlController.js
@@ -0,0 +1,34 @@
+import { nanoid } from "nanoid";
+
+import { urlModel } from "../models/index.js";
+
+export const postShortUrl = async (req, res) => {
+  const userId = res.locals.id;
+  const { url } = res.locals.url;
+  const shortUrl = nanoid(10);
+  const urlObject = {
+    url,
+    shortUrl,
+  };
+
+  try {
+    const urlIdAlreadyExists = await checkUrlAlreadyPosted(userId, url);
+    if (urlIdAlreadyExists) {
+      return res.status(409).send("Url Already Registered.");
+    }
+
+    await urlModel.insertInUrls(urlObject);
+    const urlId = await urlModel.getUrlIdByShortUrl(shortUrl);
+    await urlModel.insertInUrlsUsers(urlId, userId);
+    res.status(201).send({ shortUrl });
+  } catch (error) {
+    res
+      .status(500)
+      .send(`Internal system error.\n More details: ${error.message}`);
+  }
+};
+
+const checkUrlAlreadyPosted = async (userId, url) => {
+  const urlIdExists = await urlModel.getUrlsUsersIdByUrlAndUserId(userId, url);
+  return urlIdExists;
+};
