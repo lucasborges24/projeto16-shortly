7890ed5575d8ca0639573ea158c488cf7c6d60da
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-05 21:48:23 -0300

    feat: add controllers to /ranking GET route

--- src/routes/rankingRoute.js
+++ src/routes/rankingRoute.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 
-import { rankingMiddleware } from "../middlewares/index.js";
 import { rankingController } from "../controllers/index.js";
 
 const rankingRoutes = Router();
 
-rankingRoutes.get("/ranking", (req, res) => res.send('opa') )
+rankingRoutes.get("/ranking", rankingController.getRanking);
 
 export { rankingRoutes };
