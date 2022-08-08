a68617f2946871d81fba14049d50161d8182215e
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-08 10:55:49 -0300

    feat: create function to get user's urls

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,13 @@
 import { userModel } from "../models/index.js";
+
+export const getUserLinks = async (req, res) => {
+  const { id } = res.locals;
+  try {
+    const data = await userModel.getUserUrlsById(id);
+    res.status(200).send(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send(`Internal system error.\n More details: ${error.message}`);
+  }
+};
