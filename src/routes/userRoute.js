df9c2972b8fa0f559d716426a15494ca70759268
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-08 11:05:12 -0300

    feat: create users/me GET route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import { userController } from "../controllers/index.js";
+
+import { urlMiddleware } from "../middlewares/index.js";
 
 const userRouter = Router();
+
+userRouter.get(
+  "/users/me",
+  urlMiddleware.validateHeader,
+  urlMiddleware.checkTokenBelongsSomeUser,
+  userController.getUserLinks
+);
 
 export { userRouter };
