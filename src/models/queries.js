57eda282340f0e4aad5fcbc2be1dace662df8316
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-08 17:24:32 -0300

    fix: add left join to getUserUrls

--- src/models/queries.js
+++ src/models/queries.js
@@ -190,31 +190,34 @@ export const getRanking = () => {
 export const getUserUrls = () => {
   const query = `--sql
   SELECT
-      "users"."id",
-      "users"."name",
-      SUM(urls."visitCount") AS "visitCount",
-      json_agg(
-          json_build_object(
-              'id',
-              "urls"."id",
-              'shortUrl',
-              "urls"."shortUrl",
-              'url',
-              "urls"."url",
-              'visitCount',
-              "urls"."visitCount"
-          )
-      ) AS "shortenedUrls"
+    "users"."id",
+    "users"."name",
+    COALESCE(SUM(urls."visitCount")::int, 0) AS "visitCount",
+    CASE
+        WHEN COUNT(urls."url") > 0 THEN json_agg(
+            json_build_object(
+                'id',
+                "urls"."id",
+                'shortUrl',
+                "urls"."shortUrl",
+                'url',
+                "urls"."url",
+                'visitCount',
+                "urls"."visitCount"
+            )
+        )
+        ELSE '[]'
+    END AS "shortenedUrls"
   FROM
-      "urlsUsers"
-      JOIN users ON "urlsUsers"."userid" = "users"."id"
-      JOIN urls ON "urlsUsers"."urlId" = "urls"."id"
+    "users"
+    LEFT JOIN "urlsUsers" ON "urlsUsers"."userid" = "users"."id"
+    left JOIN urls ON "urlsUsers"."urlId" = "urls"."id"
   WHERE
-      users.id = $1
+    users.id = $1
   GROUP BY
-      "users"."id"
+    "users"."id"
   ORDER BY
-      "visitCount" DESC;
+    "visitCount" DESC;
   `;
   return query;
 };
