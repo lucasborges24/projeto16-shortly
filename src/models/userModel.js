6951553efa78e54bdade4d04e537e1eb5da4d7fe
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-08 11:04:32 -0300

    feat: create function to get user urls

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,4 +3,12 @@ import { queries } from "./index.js";
 
 // DATABASE
 
+export const getUserUrlsById = async (id) => {
+    const { rows: data } = await connection.query(queries.getUserUrls(), [id])
+    if (data && data.length !== 0) {
+        return data
+    }
+    return false;
+}
+
 // SCHEMA
